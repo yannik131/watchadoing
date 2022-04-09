c041913a59debcb7d12a2eb0f26f20264e03e363
Yannik <yannik131@web.de>
2022-04-09 18:30:18 +0200

    Added uwsgi config

--- app/src/services/axios.js
+++ app/src/services/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const getClient = function() {
     const instance = axios.create();
-    instance.defaults.baseURL = 'http://localhost:8000/';
+    instance.defaults.baseURL = 'https://app.myactivities.net/';
     
     return instance;
 }
