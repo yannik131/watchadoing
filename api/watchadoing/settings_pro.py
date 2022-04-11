96399ca51be7c7863f237b57efe8f92888e54a3a
Yannik <yannik131@web.de>
2022-04-11 21:52:18 +0200

    Fixed CORS

--- api/watchadoing/settings_pro.py
+++ api/watchadoing/settings_pro.py
@@ -2,8 +2,9 @@ from .settings import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['watchadoing.net']
+ALLOWED_HOSTS = ['watchadoing.net', 'www.watchadoing.net']
 
 CORS_ALLOWED_ORIGINS = [
-    'https://watchadoing.net'
+    'https://watchadoing.net',
+    'https://www.watchadoing.net'
 ]
