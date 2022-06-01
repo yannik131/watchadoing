e9a52ebddec47816a7999cca6f84b179b329f7a0
Yannik Schroeder <yannik131@web.de>
2022-06-01 21:28:08 +0200

    Forgot an ampersand..

--- api/up.sh
+++ api/up.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 uwsgi --ini ./config/uwsgi.ini &
-daphne -u /tmp/daphne2.sock watchadoing.asgi:application --access-log /home/projects/watchadoing/api/logs/daphne.log
+daphne -u /tmp/daphne2.sock watchadoing.asgi:application --access-log /home/projects/watchadoing/api/logs/daphne.log &
