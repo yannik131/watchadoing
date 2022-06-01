103755f950fb48a2970f8a753121a1e2c0c6c990
Yannik Schroeder <yannik131@web.de>
2022-06-01 14:38:58 +0200

    cleanup

--- app/src/helpers/positionFactory.js
+++ app/src/helpers/positionFactory.js
@@ -1,5 +1,16 @@
 import { getRandomInteger, shuffleArray } from './utils';
 
+/*
+Generates positions according to the following matrix (which extends to infinity, of course):
+
+-3,-2 -2,-2 -1,-2 0,-2 1,-2 2,-2 3,-2
+-3,-1 -2,-1 -1,-1 0,-1 1,-1 2,-1 3,-1
+-3,0  -2,0  -1,0  0,0  1,0  2,0  3,0
+-3,1  -2,1  -1,1  0,1  1,1  2,1  3,1
+-3,2  -2,2  -1,2  0,2  1,2  2,2  3,2
+
+*/
+
 let positionFactory = null;
 
 export default class PositionFactory {
