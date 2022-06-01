553aec9867af10d885368b0e5d910b2770bd0184
Yannik Schroeder <yannik131@web.de>
2022-06-01 16:55:06 +0200

    Added favicon

--- build_push.sh
+++ build_push.sh
@@ -2,4 +2,5 @@
 
 cd app
 VUE_APP_MODE=production yarn build
+ssh root@myactivities.net "rm -rf /home/projects/watchadoing/app/dist"
 scp -r dist root@myactivities.net:/home/projects/watchadoing/app/dist
