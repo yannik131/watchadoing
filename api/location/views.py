7ef3639c1ae39c3792a50000372392801802d34c
Yannik <yannik131@web.de>
2022-06-08 17:50:18 +0200

    Added more logging

--- api/location/views.py
+++ api/location/views.py
@@ -1,15 +1,29 @@
-from rest_framework import views, response
+from rest_framework import views, response, exceptions
 from rest_framework.request import Request
 from .models import Location
 from .serializers import LocationSerializer
+import logging
+import traceback
 
+logger = logging.getLogger('watchadoing')
 
 class LocationCreateView(views.APIView):
     def post(self, request: Request):
-        if request.data.get('address'):
-            location = Location.determine_from_address(request.data.get('address'))
-        else:
-            location = Location.determine_from_coordinates(**request.data['coordinates'])
+        try:
+            if request.data.get('address'):
+                location = Location.determine_from_address(request.data.get('address'))
+            elif request.data.get('coordinates'):
+                location = Location.determine_from_coordinates(**request.data['coordinates'])
+            else:
+                raise exceptions.ValidationError({'error': 'Bad request'})
+        except Exception as e:
+            if request.data.get('address'):
+                logger.warn(f'Address {request.data.get("address")} led to exception: {e}')
+            elif request.data.get('coordinates'):
+                logger.warn(f'Coordinates {request.data.get("coordinates")} led to error: {e}')
+            else:
+                logger.warn('Bad request!')
+            raise e
             
         return response.Response({'location': LocationSerializer(location).data})
     
