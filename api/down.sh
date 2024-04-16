601b03fa926a7d7b56a3068c14d8c263fed1547f
Yannik Schroeder <yannik131@web.de>
2024-04-16 21:09:10 +0000

    Added source to scripts

--- api/down.sh
+++ api/down.sh
@@ -1,2 +1,3 @@
+source env/bin/activate
 uwsgi --stop /tmp/watchadoing.pid
 ps -ef | grep daphne2 | grep sock | awk '{print $2}' | xargs kill -9
--- api/up.sh
+++ api/up.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
+source env/bin/activate
 uwsgi --ini ./config/uwsgi.ini &
 ./env/bin/daphne -u /tmp/daphne2.sock watchadoing.asgi:application --access-log /home/websites/watchadoing/api/logs/daphne.log &
